a0686c53e76c0f3783d60f9eaf497065f9ccb51c
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2021-08-23 06:57:14 +0530

    checking

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,23 +10,26 @@ import BigCrousel from "../components/homeComponents/BigCrouselDiv";
 import SeconLastSection from "../components/homeComponents/secondLastSection";
 import Footer from "../components/homeComponents/Footer";
 import { FormContext } from "../components/FormContext";
+import DrawerHome from "../components/homeComponents/DrawerHome";
 const Home = (props) => {
   const { imagesContext } = useContext(FormContext);
   return (
     <>
-      <ImageCrousel imss={props.imss} />
-      <div className="container">
-        <Section2 />
-        <Section3 />
-        <Section4 />
-        <Section4 />
-        {/* <SlidingPanel /> */}
-        <ImageCrouselCenter />
-        <BigCrousel imss={props.imss} />
-        <SeconLastSection />
-        {/* <SectionPlayground /> */}
-      </div>
-      <Footer />
+      <DrawerHome>
+        <ImageCrousel imss={props.imss} />
+        <div className="container">
+          <Section2 />
+          <Section3 />
+          <Section4 />
+          <Section4 />
+          {/* <SlidingPanel /> */}
+          <ImageCrouselCenter />
+          <BigCrousel imss={props.imss} />
+          <SeconLastSection />
+          {/* <SectionPlayground /> */}
+        </div>
+        <Footer />
+      </DrawerHome>
       {/* <div className="container-fluid mb-2"></div> */}
     </>
   );
