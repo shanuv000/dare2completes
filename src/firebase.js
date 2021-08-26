fc7a95f6564d02ab38ccba9cc335d7066b8ebbf3
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2021-08-26 23:39:15 +0530

    delete firebase.js

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDJU5uewafSYBTW5hrERBU64T3AL4Vck4Q",
-  authDomain: "react-form-68489.firebaseapp.com",
-  projectId: "react-form-68489",
-  storageBucket: "react-form-68489.appspot.com",
-  messagingSenderId: "532560138624",
-  appId: "1:532560138624:web:523ca16a2f4e16f2ddf165",
-};
